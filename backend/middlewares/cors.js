7bb76332b1b7fb360cf2bf97ae4d677bfd791590
Yulia Burenko <juliaburenko@yandex.ru>
2023-01-15 15:39:17 +0300

    for deploy

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -1,36 +1,28 @@
 const allowedCors = [
-    'http://julbrn.nomoredomains.rocks',
-    'https://julbrn.nomoredomains.rocks',
-    'https://api.julbrn.nomoredomains.rocks',
-    'http://api.julbrn.nomoredomains.rocks',
-    'localhost:3000',
-    'http://localhost:3000',
+    'http://julbrn.nomoredomains.rocks/',
+    'https://julbrn.nomoredomains.rocks/',
+    'https://api.julbrn.nomoredomains.rocks/',
+    'http://api.julbrn.nomoredomains.rocks/',
+    'localhost:3001',
+    'http://localhost:3001',
     'https://localhost:3000',
+    'localhost:3000',
+    'http://localhost:3000/',
 ];
 
 module.exports = (req, res, next) => {
-    // Сохраняем источник запроса в переменную origin
     const { origin } = req.headers;
-    // Сохраняем метод запроса в переменную method
     const { method } = req;
     const DEFAULT_ALLOWED_METHODS = 'GET, HEAD, PUT, PATCH, POST, DELETE';
     const requestHeaders = req.headers['access-control-request-headers'];
-    // Проверим, есть ли источник запроса среди разрешённых
     if (allowedCors.includes(origin)) {
         res.header('Access-Control-Allow-Origin', origin);
-        // С опцией credentials токен передает браузер, который сохраняется в куках
         res.header('Access-Control-Allow-Credentials', true);
     }
 
     if (method === 'OPTIONS') {
-        // Тогда это предварительный CORS-запрос
-        // Разрешаем кросс-доменные запросы типов, указанных в DEFAULT_ALLOWED_METHODS
         res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-        // Разрешаем кросс-доменные запросы с заголовками, указанными в requestHeaders
-        // Обычно используют значение заголовка Access-Control-Request-Headers,
-        // который передает браузер вместе с запросом
         res.header('Access-Control-Allow-Headers', requestHeaders);
-        // Завершаем обработку запроса и возвращаем результат клиенту
         return res.end();
     }
 
